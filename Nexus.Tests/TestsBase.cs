487188fff88a5513533b37d226b1db5e403c3d5a
Joe Thorpe <jthorpe@stotthoare.com.au>
2021-07-02 20:28:47 +0800

    Initial commit

---
+++ Nexus.Tests/TestsBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Nexus.Tests
+{
+    //public abstract class TestsBase : IDisposable
+    //{
+    //    protected static readonly IConfiguration Configuration = new ConfigurationBuilder().AddUserSecrets<TestsBase>().Build();
+    //    protected readonly ITestOutputHelper Output;
+    //    protected readonly ILogger Logger;
+
+    //    // Runs before all tests to provide shared context
+    //    protected TestsBase(ITestOutputHelper output)
+    //    {
+    //        this.Output = output;
+    //        Logger = output.BuildLogger();
+    //    }
+
+    //    // Runs after all tests
+    //    public void Dispose()
+    //    {
+            
+    //    }
+    //}
+}
---
+++ Server/Services/ControllerLoggingMiddleware.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace Nexus.Server.Services
+{
+    public class ControllerLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ControllerLoggingMiddleware> _logger;
+
+        public ControllerLoggingMiddleware(RequestDelegate next, ILogger<ControllerLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //First, get the incoming request
+            var request = await FormatRequest(context.Request);
+
+            //Copy a pointer to the original response body stream
+            var originalBodyStream = context.Response.Body;
+
+            //Create a new memory stream...
+            using (var responseBody = new MemoryStream())
+            {
+                //...and use that for the temporary response body
+                context.Response.Body = responseBody;
+
+                //Continue down the Middleware pipeline, eventually returning to this class
+                await _next(context);
+
+                //Format the response from the server
+                var response = await FormatResponse(context.Response);
+
+                //TODO: Save log to chosen datastore
+                var logMessage = $"{request}{response}";
+                _logger.LogDebug(logMessage);
+
+                //Copy the contents of the new memory stream (which contains the response) to the original stream, which is then returned to the client.
+                await responseBody.CopyToAsync(originalBodyStream);
+            }
+        }
+
+        private async Task<string> FormatRequest(HttpRequest request)
+        {
+            var body = request.Body;
+
+            //This line allows us to set the reader for the request back at the beginning of its stream.
+            request.EnableBuffering();
+
+            //We now need to read the request stream.  First, we create a new byte[] with the same length as the request stream...
+            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+
+            //...Then we copy the entire request stream into the new buffer.
+            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+
+            //We convert the byte[] into a string using UTF8 encoding...
+            var bodyAsText = Encoding.UTF8.GetString(buffer);
+
+            //..and finally, assign the read body back to the request body, which is allowed because of EnableRewind()
+            request.Body = body;
+
+            return
+                $">>> {request.Method} {request.Scheme}://{request.Host}{request.Path}{request.QueryString}{Environment.NewLine}{PrettyPrintJson(bodyAsText)}";
+        }
+
+        private async Task<string> FormatResponse(HttpResponse response)
+        {
+            //We need to read the response stream from the beginning...
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            //...and copy it into a string
+            string text = await new StreamReader(response.Body).ReadToEndAsync();
+
+            //We need to reset the reader for the response so that the client can read it.
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            //Return the string for the response, including the status code (e.g. 200, 404, 401, etc.)
+            return $"<<< {response.StatusCode}{Environment.NewLine}{PrettyPrintJson(text)}";
+        }
+
+        private string PrettyPrintJson(string json)
+        {
+            try
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>(json);
+                return JsonSerializer.Serialize(jsonElement, new JsonSerializerOptions { WriteIndented = true });
+            }
+            catch (JsonException)
+            {
+                return null;
+            }
+        }
+    }
+}
